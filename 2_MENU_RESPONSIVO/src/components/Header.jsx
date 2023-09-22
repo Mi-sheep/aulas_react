1acb19fcb60ca7dd65deb5e943b34823dca9c49d
Mirela <mirela.al.damasceno@gmail.com>
2023-09-22 15:29:49 -0300

    menu abre e fecha

--- 2_MENU_RESPONSIVO/src/components/Header.jsx
+++ 2_MENU_RESPONSIVO/src/components/Header.jsx
@@ -1,24 +1,38 @@
 import { List } from "@phosphor-icons/react"
+import { useState } from "react"
 
 import styles from "./Header.module.css"
 
 function Header() {
+  const [abrirMenu, setAbrirMenu] = useState(false)
+
+  function handleAbrirFecharMenu() {
+    if (abrirMenu === true) {
+      setAbrirMenu(false)
+      return
+    }
+
+    setAbrirMenu(true)
+  }
+
   return (
     <header>
-        <div className="mobile">
-            <div className={styles.menu_logo}>
-                <button><List size={32} /></button>
-                <h1>Logo</h1>
-            </div>
-            <div className={styles.opcoes}>
-              <nav>
-                <a href="">Home</a>
-                <a href="">Ferramentas</a>
-                <a href="">Objetivos</a>
-                <a href="">Bibliografia</a>
-              </nav>
-            </div>
+      <div className={styles.mobile}>
+        <div className={styles.menu_logo}>
+          <button onClick={handleAbrirFecharMenu}>
+            <List size={32} />
+          </button>
+          <h1>Logo</h1>
         </div>
+        <div className={`${styles.options} ${abrirMenu === true && styles.open}`}>
+          <nav>
+            <a href="">Home</a>
+            <a href="">Ferramentas</a>
+            <a href="">Objetivos</a>
+            <a href="">Bibliografia</a>
+          </nav>
+        </div>
+      </div>
     </header>
   )
 }
